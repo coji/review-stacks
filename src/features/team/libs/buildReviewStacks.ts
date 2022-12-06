7477c4937621a3aaf810c61a7c1170d81713e55f
coji <coji@techtalk.jp>
2022-12-06 18:55:44 +0900

    refactore: buildReviewStacks

--- src/features/team/libs/buildReviewStacks.ts
+++ src/features/team/libs/buildReviewStacks.ts
@@ -1,54 +1,68 @@
 import type { Types } from '@gitbeaker/node/dist/types'
 import { ReviewStackItem, UserInfo } from '~/interfaces/model'
 
-const addAssignMR = (
-  assignees: Map<string, Types.MergeRequestSchema[]>,
+const addMR = (
+  assigneesOrReviewers: Map<string, Types.MergeRequestSchema[]>,
   username: string,
   mr: Types.MergeRequestSchema
 ) => {
-  const exist = assignees.get(username) ?? []
-  assignees.set(username, [...exist, mr])
+  const exist = assigneesOrReviewers.get(username) ?? []
+  assigneesOrReviewers.set(username, [...exist, mr])
 }
 
-const addReviewMR = (
-  reviewers: Map<string, Types.MergeRequestSchema[]>,
-  username: string,
+const buildUserInfo = (user: Omit<Types.UserSchema, 'created_at'>) => {
+  return {
+    username: user.username,
+    name: user.name,
+    avatar: user.avatar_url,
+    assigned: [],
+    reviews: []
+  } as UserInfo
+}
+
+const buildAssigneesAndReviewrs = (
+  users: Record<string, UserInfo>,
+  assignees: Map<string, any>,
+  reviewers: Map<string, any>,
   mr: Types.MergeRequestSchema
 ) => {
-  const exist = reviewers.get(username) ?? []
-  reviewers.set(username, [...exist, mr])
+  const assignee = mr.assignee ?? mr.author
+  const assigneeUsername = String(assignee.username)
+  users[String(assignee.username)] ||= buildUserInfo(assignee)
+  addMR(assignees, assigneeUsername, mr)
+
+  if (mr.reviewers) {
+    for (const reviewer of mr.reviewers) {
+      users[String(reviewer.username)] ||= buildUserInfo(reviewer)
+      addMR(reviewers, String(reviewer.username), mr)
+    }
+  }
+}
+
+const buildReviewStackItems = (
+  users: { [key: string]: UserInfo },
+  assigneesOrReviewers: Map<string, any>
+) => {
+  return Array.from(assigneesOrReviewers.entries())
+    .map(
+      (entry) =>
+        ({
+          user: users[entry[0]],
+          mergerequests: entry[1]
+        } as ReviewStackItem)
+    )
+    .sort((a, b) => (b.mergerequests.length >= a.mergerequests.length ? 1 : -1))
 }
 
 export const buildReviewStacks = (
   mergerequests: Types.MergeRequestSchema[]
 ) => {
-  const users: { [key: string]: UserInfo } = {}
-  const assignees = new Map()
-  const reviewers = new Map()
+  const users: Record<string, UserInfo> = {}
+  const assignees = new Map<string, any>()
+  const reviewers = new Map<string, any>()
 
   for (const mr of mergerequests) {
-    const assignee = mr.assignee ?? mr.author
-    users[String(assignee.username)] ||= {
-      username: assignee.username,
-      name: assignee.name,
-      avatar: assignee.avatar_url,
-      assigned: [],
-      reviews: []
-    } as UserInfo
-    addAssignMR(assignees, String(assignee.username), mr)
-
-    if (mr.reviewers) {
-      for (const reviewer of mr.reviewers) {
-        users[String(reviewer.username)] ||= {
-          username: reviewer.username,
-          name: reviewer.name,
-          avatar: reviewer.avatar_url,
-          assigned: [],
-          reviews: []
-        } as UserInfo
-        addReviewMR(reviewers, String(reviewer.username), mr)
-      }
-    }
+    buildAssigneesAndReviewrs(users, assignees, reviewers, mr)
   }
 
   for (const username of Object.keys(users)) {
@@ -58,27 +72,7 @@ export const buildReviewStacks = (
 
   return {
     users,
-    assignees: Array.from(assignees.entries())
-      .map(
-        (entry) =>
-          ({
-            user: users[entry[0]],
-            mergerequests: entry[1]
-          } as ReviewStackItem)
-      )
-      .sort((a, b) =>
-        b.mergerequests.length >= a.mergerequests.length ? 1 : -1
-      ),
-    reviewers: Array.from(reviewers.entries())
-      .map(
-        (entry) =>
-          ({
-            user: users[entry[0]],
-            mergerequests: entry[1]
-          } as ReviewStackItem)
-      )
-      .sort((a, b) =>
-        b.mergerequests.length >= a.mergerequests.length ? 1 : -1
-      )
+    assignees: buildReviewStackItems(users, assignees),
+    reviewers: buildReviewStackItems(users, reviewers)
   }
 }
