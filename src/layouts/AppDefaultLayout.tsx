6040e40e72df08a691f84af417ae2b9c7d508810
coji <coji@techtalk.jp>
2022-12-27 20:01:38 +0900

    fix devDependencies

--- src/layouts/AppDefaultLayout.tsx
+++ src/layouts/AppDefaultLayout.tsx
@@ -1,5 +1,4 @@
 import { Box, Grid } from '@chakra-ui/react'
-import Div100vh from 'react-div-100vh'
 import { AppNavbar } from '../components/AppNavbar'
 import { AppFooter } from '../components/AppFooter'
 
@@ -9,16 +8,20 @@ interface AppDefaultLayoutProps {
 
 export const AppDefaultLayout = ({ children }: AppDefaultLayoutProps) => {
   return (
-    <Div100vh>
-      <Grid templateRows="auto 1fr auto" minH="100vh">
-        <AppNavbar />
+    <Grid
+      templateRows="auto 1fr auto"
+      css={`
+        height: 100vh;
+        height: 100dvh;
+      `}
+    >
+      <AppNavbar />
 
-        <Box as="main" flexGrow="1" overflow="auto" p={{ base: '0', sm: '2' }}>
-          {children}
-        </Box>
+      <Box as="main" flexGrow="1" overflow="auto" p={{ base: '0', sm: '2' }}>
+        {children}
+      </Box>
 
-        <AppFooter />
-      </Grid>
-    </Div100vh>
+      <AppFooter />
+    </Grid>
   )
 }
